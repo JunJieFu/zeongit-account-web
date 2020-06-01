182b9752ea9aa9788241560711eaf37d78688196
245142637@qq.com <asd+-*/00>
2020-06-01 17:40:59 +0800

    .

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Vuetify from "vuetify/lib"
 import zhHans from "vuetify/es5/locale/zh-Hans"
+import storageUtil from "../assets/script/util/storage"
 Vue.use(Vuetify)
 
 export default new Vuetify({
@@ -10,5 +11,19 @@ export default new Vuetify({
   lang: {
     locales: { zhHans },
     current: "zhHans"
+  },
+  theme: {
+    dark: storageUtil.localGet("account::menu::dark", false),
+    themes: {
+      light: {
+        primary: "#0B99FF",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF0000",
+        info: "#1976D2",
+        success: "#3CB371",
+        warning: "#FBA207"
+      }
+    }
   }
 })
